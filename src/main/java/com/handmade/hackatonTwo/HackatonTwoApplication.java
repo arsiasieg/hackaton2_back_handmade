bb6ba83ae78d1783d22df495bea95b2f0626dd18
Arsia <cam.lucas89@gmail.com>
2022-01-19 09:34:25 +0100

    Initialization project

---
+++ src/main/java/com/handmade/hackatonTwo/HackatonTwoApplication.java
@@ -0,0 +1,13 @@
+package com.handmade.hackatonTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handmade/hackatonTwo/HackatonTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handmade.hackatonTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
