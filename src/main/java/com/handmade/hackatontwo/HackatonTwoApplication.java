fb17fa4b8e3e048cfde8c3e91a54dc3a1bd9e236
camille <camfrelat@gmail.com>
2022-01-20 11:46:06 +0100

    first commit

--- src/main/java/com/handmade/hackatontwo/HackatonTwoApplication.java
+++ src/main/java/com/handmade/hackatontwo/HackatonTwoApplication.java
@@ -1,4 +1,4 @@
-package com.handmade.hackatonTwo;
+package com.handmade.hackatontwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
